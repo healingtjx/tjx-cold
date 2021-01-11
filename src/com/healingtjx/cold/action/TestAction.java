0e4069ce5f77cf5226af87e07e65f1db797655db
tjx1014 <1095288440@qq.com>
2021-01-11 11:10:52 +0800

    build : 首次构建项目

---
+++ src/com/healingtjx/cold/action/TestAction.java
@@ -0,0 +1,17 @@
+package com.healingtjx.cold.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @Author: tjx
+ * @Description:
+ * @Date: 创建于10:59 2021-01-11
+ **/
+public class TestAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
