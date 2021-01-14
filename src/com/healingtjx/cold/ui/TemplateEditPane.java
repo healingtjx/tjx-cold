cf619c98673535d66bb8f7b5d9aa3bcc041eeea7
tjx1014 <1095288440@qq.com>
2021-01-14 11:15:00 +0800

    feat : 增加 jtextarea

--- src/com/healingtjx/cold/ui/TemplateEditPane.java
+++ src/com/healingtjx/cold/ui/TemplateEditPane.java
@@ -1,6 +1,7 @@
 package com.healingtjx.cold.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @Author: tjx
@@ -8,21 +9,32 @@ import javax.swing.*;
  * @Date: 创建于11:28 2021-01-13
  **/
 public class TemplateEditPane {
-    private JPanel     templateEdit;
+    private JPanel templateEdit;
     private JTextField templateNameText;
-    private JSpinner   classNumberSpinner;
+    private JSpinner classNumberSpinner;
     private JTextField classNameText;
     private JTextField fileEncodingText;
-    private JComboBox  templateLanguage;
-    private JButton    testButton;
-    private JComboBox  testInputs;
+    private JComboBox templateLanguage;
+    private JButton testButton;
+    private JComboBox testInputs;
     private JSplitPane editorSplitPane;
     private JTextArea textArea1;
+    private JScrollPane scrollPane;
+    private JPanel panel1;
     private JButton showTestInput;
     private JComboBox targetLanguage;
+
 
 
     public JPanel getTemplateEdit() {
         return templateEdit;
     }
+
+
+    private void createUIComponents() {
+        // 手动创建 jTextArea
+        textArea1 = new JTextArea(40,1);
+    }
+
+
 }
